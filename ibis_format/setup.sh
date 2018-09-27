5521c7b4eb56b21aee9c304bb2dc4a71c3e54849
ran <mochimochi.morst@gmail.com>
2018-09-27 16:42:10 +0900

    add ibis_format/setup.sh

---
+++ ibis_format/setup.sh
@@ -0,0 +1,1 @@
+tar -zxvf ieice_tech.tar.gz
--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 mkdir ibis_format
 curl http://www.ieice.org/ftp/tex/tech_rep/LaTeX2e/ieice_tech.tar.gz > ibis_format/ieice_tech.tar.gz
 curl http://www.ieice.org/ftp/tex/tech_rep/LaTeX2e/readme2ej.pdf > ibis_format/readme2ej.pdf
+
+cd ibis_format
+tar -zxvf ieice_tech.tar.gz
