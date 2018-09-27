3540d6b75458b5f556752ef6992dcc77ca233322
ran <mochimochi.morst@gmail.com>
2018-09-27 19:32:16 +0900

    add workspace

--- ibis_format/setup.sh
+++ ibis_format/setup.sh
@@ -1,1 +1,8 @@
 tar -zxvf ieice_tech.tar.gz
+cp -r latex2e/UNIXSJIS ieice_tech_utf8
+nkf -w --overwrite ieice_tech_utf8/*
+rm -r latex2e
+
+cp ieice_tech_utf8/ieicej.cls ../workspace/
+cp ieice_tech_utf8/template.tex ../workspace/
+touch main.tex
