51b027f37d0dfb9b146ce6fdde3ac1aacdc44591
ran <mochimochi.morst@gmail.com>
2018-09-27 16:35:32 +0900

    initial commit

---
+++ setup.sh
@@ -0,0 +1,3 @@
+mkdir ibis_format
+curl http://www.ieice.org/ftp/tex/tech_rep/LaTeX2e/ieice_tech.tar.gz > ibis_format/ieice_tech.tar.gz
+curl http://www.ieice.org/ftp/tex/tech_rep/LaTeX2e/readme2ej.pdf > ibis_format/readme2ej.pdf
